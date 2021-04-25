58a0314bde604d993c172ecda1d582c051b37455
Clemence-jzq <zheqin.jiang@gmail.com>
2021-04-26 01:12:49 +0200

    menu
    main menu, pause menu, credit menu et character menu and setting menu

---
+++ IC06/Assets/Scripts/Caracter/CharacterSelect.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    private int index = 0;
+    public GameObject[] characterPrefabs;
+    private GameObject[] characterShow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterShow = new GameObject[characterPrefabs.Length];
+
+        for (int i = 0; i < characterPrefabs.Length; i++)
+        {
+            characterShow[i] = (GameObject)Instantiate(characterPrefabs[i], transform.position, transform.rotation);
+        }
+
+        ChangeCharacter(index);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            index += 1;
+
+            if (index >= characterPrefabs.Length)
+                index = 0;
+
+            ChangeCharacter(index);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            index -= 1;
+
+            if (index < 0)
+                index = characterPrefabs.Length - 1;
+
+            ChangeCharacter(index);
+        }
+    }
+
+    void ChangeCharacter(int x)
+    {
+        for (int i = 0; i < characterPrefabs.Length; i++)
+        {
+            if (i == x)
+                //show
+                characterShow[i].SetActive(true);
+            else
+                //invisible
+                characterShow[i].SetActive(false);
+        }
+    }
+}
