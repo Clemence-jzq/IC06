c11612c39cfd04dc83272cfbcc860718516e659b
Clemence-jzq <zheqin.jiang@gmail.com>
2021-04-25 17:12:10 +0200

    menu
    main menu, pause menu, credit menu et character menu and setting menu

---
+++ IC06/Assets/Scripts/Menu/Bgm.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bgm : MonoBehaviour
+{
+    public GameObject obj = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.FindGameObjectWithTag("bgm");
+        if(obj == null)
+        {
+            obj = (GameObject)Instantiate(obj);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ IC06/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void GoToSettingMenu()
+    {
+        SceneManager.LoadScene("SettingMenu");
+    }
+
+    public void GoToCreditMenu()
+    {
+        SceneManager.LoadScene("CreditMenu");
+    }
+
+    public void GotoMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("PauseMenu");
+        }
+    }
+}
